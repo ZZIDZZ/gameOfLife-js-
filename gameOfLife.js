437e5456ad1872959dd34bb7161c791022278daf
ZZIDZZ <78781000+ZZIDZZ@users.noreply.github.com>
2021-06-24 20:22:44 +0700

    Update gameOfLife.js

--- gameOfLife.js
+++ gameOfLife.js
@@ -10,7 +10,7 @@ rows = canvas.height / cellSize;
 let origins = make2dArray(cols, rows);
 let next = make2dArray(cols, rows);
 let game = true;
-let density = 0.5;
+let density = 10;
 
 
 function make2dArray(coll, row) {
@@ -52,22 +52,21 @@ initialize();
 function draw(row, coll, val) {
     ctx.strokeStyle = 'black';
     ctx.strokeRect(cellSize * coll, cellSize * row, cellSize, cellSize);
-    if (val == 1) {
+    if (val == 1 || val==2) {
         ctx.fillStyle = 'black';
-        ctx.fillRect(cellSize * coll, cellSize * row, cellSize, cellSize);
     }
     else if (val == 0) {
         ctx.fillStyle = 'white';
-        ctx.fillRect(cellSize * coll, cellSize * row, cellSize, cellSize);
     }
-    
+    ctx.fillRect(cellSize * coll, cellSize * row, cellSize, cellSize);
 }
+
 function countNeighbors(row, coll){
     let i,j,sum=0;
     for(i=-1;i<=1;i++){
         for(j=-1;j<=1;j++){
-            if(!(i==0&&j==0)){
-                sum+=origins[row+i][coll+j];
+            if(!(i==0&&j==0) && origins[row+i][coll+j] != 0){
+                sum++;
             }
         }
     }
@@ -101,7 +100,6 @@ document.onkeydown = function (e) {
 };
 
 function ruleOfLife(){
-    console.log('duar');
     for(let i=1;i<rows-1; i++){
         for(let j=1;j<cols-1;j++){
             sum = countNeighbors(i,j);
@@ -111,9 +109,13 @@ function ruleOfLife(){
                 if(sum < 2 || sum > 3){
                     next[i][j] = 0;
                 }
-                else next[i][j]=1;
+                else {
+                    next[i][j]=2;
+                }
             } 
-            else next[i][j]=0;
+            else if (origins[i][j] == 2){
+                next[i][j]=0;
+            }
         }
     }
     origins = next;
